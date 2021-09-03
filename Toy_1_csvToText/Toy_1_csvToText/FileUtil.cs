debe157d92dc7af3507eaca074889405807b6134
d-gun <daegeungim7301@gmail.com>
2021-09-04 01:31:17 +0900

    Toy PJ - csv To Text 1.0

---
+++ Toy_1_csvToText/Toy_1_csvToText/FileUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Toy_1_csvToText
+{
+    class FileUtil
+    {
+        public string ReadFileToString(FileInfo[] fileInfo, string fileName)
+        {
+            string tmp = "";
+            try
+            {
+                string parentDirPath = fileInfo[0].DirectoryName;
+                //파일 없으면 텍스트 파일 생성
+                
+
+
+                if (!File.Exists(parentDirPath + "\\" + fileName))
+                {
+                    Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " There Is No File\r\n" + "msg.txt File Created");
+                    File.WriteAllText(parentDirPath + "\\msg.txt", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " There Is No File");
+                    File.Create(parentDirPath + "\\FailFlag");
+                }
+                //있다면 파일 읽어오기
+                else
+                {
+                    tmp = File.ReadAllText(parentDirPath + "\\" + fileName);
+                    File.Delete(parentDirPath + "\\FailFlag");
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return tmp;
+        }
+
+        public FileInfo[] getFileList(string filepath)
+        {
+            DirectoryInfo di = new DirectoryInfo(filepath);
+            FileInfo[] fileInfo = di.GetFiles();
+            return fileInfo;
+        }
+    }
+}
---
+++ Toy_1_csvToText/Toy_1_csvToText/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toy_1_csvToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File클래스 생성
+            //FileStream fs = File.Create("test.txt");
+            //폴더 내의 파일 리스트 얻기.
+            FileUtil fu = new FileUtil();
+            Console.WriteLine("FilePath : " + Dic.FilePath);
+            Console.WriteLine("ConnectString : " + Dic.ConnectionString);
+            FileInfo[] fileInfoInDir = fu.getFileList(Dic.FilePath);
+            string result = fu.ReadFileToString(fileInfoInDir, "Test.txt");
+            Console.WriteLine(result);
+            //파일 리스트를 바탕으로 .csv파일 내 data 가공하여 string[]로 반환
+            //string[] 의 내용을 가공하여 .txt로 export
+
+        }
+    }
+}
